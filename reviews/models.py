364cfaba3736a47329f2d8564dea6f8a484d16f3
prabhrajsingh <500076519@stu.upes.ac.in>
2022-06-25 21:13:39 +0530

    updated the login page

--- reviews/models.py
+++ reviews/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.db import connections
 
 # Create your models here.
 class Review(models.Model):
+    
     title = models.TextField()
     content = models.TextField()
     
--- trydjango/settings.py
+++ trydjango/settings.py
@@ -81,6 +81,17 @@ WSGI_APPLICATION = 'trydjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {  
+    #     'ENGINE': 'django.db.backends.mysql',  
+    #     'NAME': 'mywatchlist',  
+    #     'USER': 'root',  
+    #     'PASSWORD': 'DJr38w@1',  
+    #     'HOST': '127.0.0.1',  
+    #     'PORT': '3306',  
+    #     'OPTIONS': {  
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+    #     }  
+    # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
